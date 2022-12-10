16af0c8ede2757e13c4acf2b6066c0e6990cf1b9
Gregory-JP <63425849+Gregory-JP@users.noreply.github.com>
2022-12-10 11:11:56 -0300

    routes fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-
+import App from './App'
 import './index.css'
-
-import { BrowserRouter } from "react-router-dom"
-import { Login } from './pages/Login/Login'
-import { Register } from './pages/Register/Register'
-
+import {BrowserRouter} from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>    
-    {/* <Register /> */}
-    <Login />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>    
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
