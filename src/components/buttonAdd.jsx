a0f93f67d79c10d171565719821ff31bd76723d0
Alan Dorneles <alanricardo.97@gmail.com>
2022-12-15 16:30:36 -0300

    button criar campeonato

---
+++ src/components/buttonAdd.jsx
@@ -0,0 +1,11 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import './buttonAdd.css'
+function buttonAdd(){
+  return (
+      <button className="add">
+        <AiOutlinePlus />
+      </button>
+
+  );
+}
+export default buttonAdd;
