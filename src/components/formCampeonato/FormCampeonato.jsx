fd3866eea8e9d04af07c8af592308423bd047b62
Alan Dorneles <alanricardo.97@gmail.com>
2022-12-15 16:02:47 -0300

    formulário com inputs de endereço  e auto preenchimento com a API via cep

--- src/components/formCampeonato/FormCampeonato.jsx
+++ src/components/formCampeonato/FormCampeonato.jsx
@@ -5,21 +5,54 @@ import { Form, FormGroup, Label, Input, FormText, Button } from "reactstrap";
 import { BiDollar, BiCalendar, BiMap } from "react-icons/bi";
 import { BsPeople } from "react-icons/bs";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import logo from "./aqua_padel.png";
+import { useForm } from "react-hook-form";
 
 function FormCampeonato() {
+  const { register, handleSubmit, setValue } = useForm();
+  const onSubmit = (e) => {
+    console.log(e);
+  };
+  const checkCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, "");
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        const logradouro = document.querySelector("#logradouro");
+        const cidade = document.querySelector("#cidade");
+        const bairro = document.querySelector("#bairro");
+        const uf = document.querySelector("#uf");
+        const city_uf = document.getElementById("city_uf");
+
+        function setaCidadeEstado(valor) {
+          if (typeof valor === "undefined") {
+            city_uf.innerHTML = "error";
+          } else city_uf.innerText = data.localidade + "/" + data.uf;
+        }
+        setaCidadeEstado(city_uf);
+
+        logradouro.value = data.logradouro;
+        cidade.value = data.localidade;
+        bairro.value = data.bairro;
+        uf.value = data.uf;
+        setValue("logradouro", data.logradouro);
+        setValue("cidade", data.localidade);
+        setValue("bairro", data.bairro);
+        setValue("uf", data.uf);
+      });
+  };
   const [image, setImage] = useState("");
-  const [endImg] = useState("./padel-thumbnail.jpg");
+  const [endImg] = useState({ logo });
   const [dataIni, setDataIni] = useState("");
   const [dataEnd, setDataEnd] = useState("");
   const [dupla, setDupla] = useState(0);
-  const [endereco, setEndereco] = useState("");
   const [valor, setValor] = useState("");
 
   return (
     <div className="container_principal">
       <div className="container_formulario">
         <div className="formulario">
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <FormGroup>
               <Label for="inicio_campeonato">Inicio do Campeonato *</Label>
               <Input
@@ -34,7 +67,6 @@ function FormCampeonato() {
               <Label for="termino_campeonato">Término do campeonato *</Label>
               <Input
                 id="termino_campeonato"
-                name="termino"
                 placeholder="01/01/1990"
                 type="date"
                 value={dataEnd}
@@ -61,7 +93,6 @@ function FormCampeonato() {
                   <Label for="nr_duplas">Nrº de duplas *</Label>
                   <Input
                     id="nr_duplas"
-                    name="nr_duplas"
                     type="select"
                     value={dupla}
                     onChange={(e) => setDupla(e.target.value)}
@@ -79,31 +110,14 @@ function FormCampeonato() {
                 </FormGroup>
                 <FormGroup className="quadra">
                   <Label for="nr_quadra">Quadras *</Label>
-                  <Input
-                    id="nr_quadras"
-                    name="nr_quadras"
-                    placeholder="4"
-                    type="number"
-                  />
+                  <Input id="nr_quadras" placeholder="4" type="number" />
                 </FormGroup>
               </div>
             </div>
-            <FormGroup>
-              <Label for="cep">Localização*</Label>
-              <Input
-                id="cep"
-                name="cep"
-                placeholder="Rua A , 888"
-                type="text"
-                value={endereco}
-                onChange={(e) => setEndereco(e.target.value)}
-              />
-            </FormGroup>
             <FormGroup className="quadra">
-              <Label for="nr_quadra">Valor *</Label>
+              <Label for="valor">Valor *</Label>
               <Input
                 id="valor"
-                name="valor"
                 placeholder="10"
                 type="number"
                 value={valor}
@@ -120,9 +134,56 @@ function FormCampeonato() {
               />
               <FormText>dimensões de 250 x 250 *</FormText>
             </FormGroup>
-            <div className="btn_next">
-              <Button className="next_btn">Proximo</Button>
-              <AiOutlineArrowRight />
+            <FormGroup className="quadra">
+              <Label for="cep">CEP*</Label>
+              <Input
+                id="cep"
+                placeholder="99999-999"
+                type="text"
+                {...register("cep")}
+                onBlur={checkCEP}
+              />
+            </FormGroup>
+            <FormGroup className="quadra">
+              <Label for="logradouro">Logradouro</Label>
+              <Input id="logradouro" type="text" {...register("logradouro")} />
+            </FormGroup>
+            <FormGroup className="quadra">
+              <Label for="bairro">Bairro *</Label>
+              <Input
+                id="bairro"
+                placeholder="Centro"
+                type="text"
+                {...register("bairro")}
+              />
+            </FormGroup>
+            <FormGroup className="quadra">
+              <Label for="complemento">Complemento</Label>
+              <Input id="complemento" placeholder="BL 1 Ap 101" type="text" />
+            </FormGroup>
+            <FormGroup className="numero_estabelecimento">
+              <Label for="nr_estabelecimento">Número*</Label>
+              <Input id="nr_estabelecimento" placeholder="881" type="text" />
+            </FormGroup>
+            <div className="cidade_uf">
+              <FormGroup className="cidade">
+                <Label for="cidade">Cidade*</Label>
+                <Input
+                  id="cidade"
+                  placeholder="Rio Grande"
+                  type="text"
+                  {...register("cidade")}
+                />
+              </FormGroup>
+              <FormGroup className="uf " {...register("uf")}>
+                <Label for="uf">UF*</Label>
+                <Input id="uf" placeholder="RS" type="text" />
+              </FormGroup>
+            </div>
+            <div className="btn_register">
+              <Button className="register_btn" type="submit">
+                Criar Torneio
+              </Button>
             </div>
           </Form>
         </div>
@@ -138,7 +199,7 @@ function FormCampeonato() {
                 />
               ) : (
                 <img
-                  src={endImg}
+                  src={logo}
                   alt="photo-championship"
                   className="photo_championship"
                 />
@@ -153,11 +214,11 @@ function FormCampeonato() {
               </div>
               <div>
                 <BiMap className="icon_card" />
-                <p>{endereco}</p>
+                <p id="city_uf"></p>
               </div>
               <div>
                 <BiDollar className="icon_card" />
-                <p>R$ {valor}</p>
+                <p> {valor}</p>
               </div>
               <div>
                 <BsPeople className="icon_card" />
